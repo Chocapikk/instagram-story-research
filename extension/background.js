91b29e5d48bc3dcef54079c4fb2632da55bcab18
Chocapikk <balgogan@protonmail.com>
2026-04-21 18:32:03 +0200

    Fix: Clean debug logs, DM read blocking working via SharedWorker injection

--- extension/background.js
+++ extension/background.js
@@ -303,7 +303,6 @@ browser.webRequest.onBeforeSendHeaders.addListener(
 
     // Block DM read validation via header
     const friendlyName = headers["X-FB-Friendly-Name"] || "";
-    if (friendlyName.includes("MarkThread") || friendlyName.includes("DMRead")) bglog("[HDR] " + friendlyName);
     if (settings.blockDMRead && friendlyName.includes("MarkThreadAsRead")) {
       blockedCount++;
       bglog("Blocked DMRead #" + blockedCount + " (" + friendlyName + ")");
@@ -733,15 +732,6 @@ browser.webRequest.onBeforeRequest.addListener(
   ["blocking"]
 );
 
-// Debug: log ALL requests that match worker patterns
-browser.webRequest.onBeforeRequest.addListener(
-  (details) => {
-    if (details.url.includes("worker") || details.url.includes("init_script")) {
-      bglog("[WORKER URL] " + details.type + " " + details.url.substring(0, 120));
-    }
-  },
-  { urls: ["https://www.instagram.com/*"] }
-);
 
 // ---------------------------------------------------------------------------
 // Message handler
